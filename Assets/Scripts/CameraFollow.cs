fe73c786d9d210d16033b917efd478ccab3a0389
JerryLin1 <jerreelynn@gmail.com>
2020-06-22 18:44:22 -0400

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Update is called once per frame
+    public Transform leader;
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(leader.position.x, leader.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float movementSpeed = 10f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update() {
+        Move();
+        faceMouse();
+    }
+    void Move() {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical"); 
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+    void faceMouse() {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+    }
+}
