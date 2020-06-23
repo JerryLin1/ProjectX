2f88b37771b423414a5723c17649ccb7639c1365
JerryLin1 <jerreelynn@gmail.com>
2020-06-23 11:31:01 -0400

    animations added to player

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,15 +6,34 @@ public class PlayerControl : MonoBehaviour
 {
     private float movementSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
     Vector2 movement;
 
     void Update() {
+        checkInput();
+    }
+
+    void FixedUpdate() {
         Move();
     }
-    void Move() {
+    void checkInput() {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical"); 
-        //CHANGE THIS TO VELOCITY
+    }
+    
+    void Move() {
         rb.velocity = movement * movementSpeed;
+        if (movement.x != 0 || movement.y != 0) {
+            animator.SetBool("isMoving", true);
+        }
+        else {
+            animator.SetBool("isMoving", false);
+        }
+        if (movement.x > 0) {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (movement.x < 0) {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
