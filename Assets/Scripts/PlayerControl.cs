a3f65f34ee94cb2e7de7d5c5545fbbf154063478
Tom Han <hantom0127@gmail.com>
2020-06-24 22:15:33 -0400

    Added idle animations for purple guy in different directions

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,6 +9,8 @@ public class PlayerControl : MonoBehaviour
     public Animator animator;
     Vector2 movement;
 
+    KeyCode lastKeyHit;
+
     void Update() {
         checkInput();
     }
@@ -24,11 +26,12 @@ public class PlayerControl : MonoBehaviour
     void Move() {
         rb.velocity = movement * movementSpeed;
         animator.SetBool("horizontal", (movement.x != 0 && movement.y == 0) ? true : false);
+        animator.SetInteger("idle", (movement.x != 0 && movement.y == 0) ? 0 : animator.GetInteger("idle"));
         animator.SetBool("up", (movement.y > 0) ? true : false);
+        animator.SetInteger("idle", (movement.y > 0) ? 1 : animator.GetInteger("idle"));
         animator.SetBool("down", (movement.y < 0) ? true : false);
+        animator.SetInteger("idle", (movement.y < 0) ? -1 : animator.GetInteger("idle"));
 
         if (movement.x != 0) transform.localRotation = (movement.x > 0) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0,0,0);
-        
-        
     }
 }
