711dff7c740d7f6fa557bacfeb5a0fbd62df9397
Tom Han <hantom0127@gmail.com>
2020-06-25 15:31:22 -0400

    Added basic attack and basic attack animations for player

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,8 +7,8 @@ public class PlayerControl : MonoBehaviour
     private float movementSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
-    public GameObject bullet;
     Vector2 movement;
+    bool isAttacking = false;
 
     void Update() {
         checkInput();
@@ -20,15 +20,39 @@ public class PlayerControl : MonoBehaviour
     void checkInput() {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical"); 
+
+        // If the player is basic attacking:
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+            animator.SetBool("attacking", true);
+            isAttacking = true;
+            transform.localRotation = (mousePos.x >= transform.position.x) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0,0,0);
+        }
     }
+
+    void finishAttacking() {
+        animator.SetBool("attacking", false);
+        isAttacking = false;
+    }
+
     void Move() {
-        rb.velocity = movement * movementSpeed;
-        animator.SetBool("horizontal", (movement.x != 0 && movement.y == 0) ? true : false);
-        animator.SetInteger("idle", (movement.x != 0 && movement.y == 0) ? 0 : animator.GetInteger("idle"));
-        animator.SetBool("up", (movement.y > 0) ? true : false);
-        animator.SetInteger("idle", (movement.y > 0) ? 1 : animator.GetInteger("idle"));
-        animator.SetBool("down", (movement.y < 0) ? true : false);
-        animator.SetInteger("idle", (movement.y < 0) ? -1 : animator.GetInteger("idle"));
+        if (!isAttacking) {
+            rb.velocity = movement * movementSpeed;
+            animator.SetBool("horizontal", (movement.x != 0 && movement.y == 0) ? true : false);
+            animator.SetInteger("idle", (movement.x != 0 && movement.y == 0) ? 0 : animator.GetInteger("idle"));
+            animator.SetBool("up", (movement.y > 0) ? true : false);
+            animator.SetInteger("idle", (movement.y > 0) ? 1 : animator.GetInteger("idle"));
+            animator.SetBool("down", (movement.y < 0) ? true : false);
+            animator.SetInteger("idle", (movement.y < 0) ? -1 : animator.GetInteger("idle"));
+        } else {
+            rb.velocity = new Vector2(0,0);
+            animator.SetBool("up", false);
+            animator.SetBool("horizontal", false);
+            animator.SetBool("down", false);
+        }
+        
+    
 
         if (movement.x != 0) transform.localRotation = (movement.x > 0) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0,0,0);
     }
