272f5becd9e4c3a4103564f4553c4923aa06bdb9
Tom Han <hantom0127@gmail.com>
2020-06-26 00:25:36 -0400

    Added hover button for ability HDU

---
+++ Assets/Hover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public GameObject hoverPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hoverPanel.SetActive(false);
+    }
+    public void OnMouseOver() {
+        hoverPanel.SetActive(true);
+    }
+
+    public void OnMouseExit() {
+        hoverPanel.SetActive(false);
+    }
+
+   
+}
