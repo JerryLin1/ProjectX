81d00c5d6bee5610be2b6105835718b3507a2353
JerryLin1 <jerreelynn@gmail.com>
2020-06-26 20:25:59 -0400

    photon removed from objects. it is still in the folder, remove later

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -1,31 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
-public class BulletControl : MonoBehaviourPun, IPunObservable
+public class BulletControl : MonoBehaviour
 {
     public Rigidbody2D rb;
     void Update()
     {
-        
+
     }
 
-    
-    void OnCollisionEnter2D(Collision2D collisionObject) {
-       
-        Destroy(gameObject);
-        
-    }
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+
+    void OnCollisionEnter2D(Collision2D collisionObject)
     {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(rb.velocity);
-        }
-        else if (stream.IsReading)
-        {
-            rb.velocity = (Vector2)stream.ReceiveNext();
-        }
+
+        Destroy(gameObject);
+
     }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,14 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
-public class PlayerControl : MonoBehaviourPun, IPunObservable
+public class PlayerControl : MonoBehaviour
 {
     private float movementSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
-    public PhotonView pv;
     public GameObject bulletPrefab;
     Vector2 direction;
     Vector2 movement;
@@ -19,12 +17,7 @@ public class PlayerControl : MonoBehaviourPun, IPunObservable
 
     void Start()
     {
-        if (pv.IsMine)
-        {
-            Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
-        }
-
-        PhotonNetwork.OfflineMode = true; // false;
+        Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
     }
     void Update()
     {
@@ -42,7 +35,7 @@ public class PlayerControl : MonoBehaviourPun, IPunObservable
         movement.y = Input.GetAxis("Vertical");
 
         // If the player is basic attacking, set projectiles to travel to mouse location
-        if (pv.IsMine && Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
@@ -59,38 +52,38 @@ public class PlayerControl : MonoBehaviourPun, IPunObservable
 
     void Move()
     {
-        if (pv.IsMine)
+
+        // The player can move while they're not attacking
+        // Otherwise, they're forced to stay in position
+        if (!isAttacking)
         {
-            // The player can move while they're not attacking
-            // Otherwise, they're forced to stay in position
-            if (!isAttacking)
-            {
-                rb.velocity = movement * movementSpeed;
-                animator.SetBool("horizontal", (movement.x != 0 && movement.y == 0) ? true : false);
-                animator.SetInteger("idle", (movement.x != 0 && movement.y == 0) ? 0 : animator.GetInteger("idle"));
-                animator.SetBool("up", (movement.y > 0) ? true : false);
-                animator.SetInteger("idle", (movement.y > 0) ? 1 : animator.GetInteger("idle"));
-                animator.SetBool("down", (movement.y < 0) ? true : false);
-                animator.SetInteger("idle", (movement.y < 0) ? -1 : animator.GetInteger("idle"));
-            }
-            else
-            {
-                rb.velocity = new Vector2(0, 0);
-                animator.SetBool("up", false);
-                animator.SetBool("horizontal", false);
-                animator.SetBool("down", false);
-            }
+            rb.velocity = movement * movementSpeed;
+            animator.SetBool("horizontal", (movement.x != 0 && movement.y == 0) ? true : false);
+            animator.SetInteger("idle", (movement.x != 0 && movement.y == 0) ? 0 : animator.GetInteger("idle"));
+            animator.SetBool("up", (movement.y > 0) ? true : false);
+            animator.SetInteger("idle", (movement.y > 0) ? 1 : animator.GetInteger("idle"));
+            animator.SetBool("down", (movement.y < 0) ? true : false);
+            animator.SetInteger("idle", (movement.y < 0) ? -1 : animator.GetInteger("idle"));
         }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+            animator.SetBool("up", false);
+            animator.SetBool("horizontal", false);
+            animator.SetBool("down", false);
+        }
+
         if (rb.velocity.x != 0) transform.localRotation = (rb.velocity.x > 0) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
     }
     void Shoot()
     {
         // If the player is basic attacking, a projectile is released but the attack cooldown starts
-        if (timer == 0) {
-            if (pv.IsMine && Input.GetMouseButton(0))
-            {            
-                GameObject bullet = (PhotonNetwork.OfflineMode) ? Instantiate(bulletPrefab, transform.position, Quaternion.identity) : PhotonNetwork.Instantiate(bulletPrefab.name, transform.position, Quaternion.identity);
-                Physics2D.IgnoreCollision(gameObject.GetComponent<BoxCollider2D>(), bullet.GetComponent<CapsuleCollider2D>()); 
+        if (timer == 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                Physics2D.IgnoreCollision(gameObject.GetComponent<BoxCollider2D>(), bullet.GetComponent<CapsuleCollider2D>());
                 bullet.GetComponent<Rigidbody2D>().velocity = direction * 20f;
                 Destroy(bullet, 1f);
 
@@ -99,22 +92,15 @@ public class PlayerControl : MonoBehaviourPun, IPunObservable
                 transform.localRotation = (mousePos.x >= transform.position.x) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
                 timer = 0.01f;
             }
-        } else if (timer > waitTime) {
+        }
+        else if (timer > waitTime)
+        {
             timer = 0;
-        } else {
+        }
+        else
+        {
             timer += Time.deltaTime;
         }
-        
-    }
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(rb.velocity);
-        }
-        else if (stream.IsReading)
-        {
-            rb.velocity = (Vector2)stream.ReceiveNext();
-        }
+
     }
 }
