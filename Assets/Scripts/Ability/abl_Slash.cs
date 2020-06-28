062af179a46fda22d87bf1e7ad569adc7bb0327f
Tom Han <hantom0127@gmail.com>
2020-06-28 19:16:43 -0400

    Added attack projectile

--- Assets/Scripts/Ability/abl_Slash.cs
+++ Assets/Scripts/Ability/abl_Slash.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class abl_Slash : Ability
 {
     protected override float cooldown { get { return 0.5f; } }
+    public GameObject crescentPrefab;
     public override void Cast(Vector3 mousePos, Vector2 direction)
     {
-        
+        GameObject crescent = Instantiate(crescentPrefab, parent.transform.position, parent.localRotation);
+        if (mousePos.x > parent.transform.position.x) {
+            crescent.transform.localScale = new Vector2(crescent.transform.localScale.x * -1, crescent.transform.localScale.y);
+        }
+        crescent.GetComponent<Rigidbody2D>().AddForce(direction*500f);
+        crescent.transform.up = direction;
+        Destroy(crescent, 0.2f);
+
         animator.SetTrigger("shoot");
         parent.GetComponent<Transform>().localRotation = (mousePos.x >= transform.position.x) ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
         goOnCooldown();
