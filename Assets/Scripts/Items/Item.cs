1224edbb4ae73d6714098c7793541ee906abc159
JerryLin1 <jerreelynn@gmail.com>
2020-07-18 11:36:31 -0400

    soul lantern

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -13,8 +13,10 @@ public abstract class Item : MonoBehaviour
     public virtual void onPickUpEffect(Entity control) {}
     public virtual void passiveEffect(Entity control) {}
     public virtual void onDamagedEffect(Entity control, Entity source) {}
+    public virtual void onHealEffect(Entity control) {}
     public virtual void onAbility(Entity control) {}
     public virtual void onHitEffect(Entity control, Entity enemy) {}
+    public virtual void onKillEffect (Entity control) {}
     public Sprite getItemSprite() {
         return transform.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>().sprite;
     }
---
+++ Assets/item_soullantern.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item_soullantern : Item
+{
+    public float healAmount = 10;
+    void Start()
+    {
+        itemTier = 2;
+        itemName = "Soul Lantern";
+        itemDesc = "Killing enemies heals you for a small amount.";
+        itemType = "passive";
+    }
+    public override void onKillEffect(Entity control) {
+        control.heal(healAmount);
+    }
+}
