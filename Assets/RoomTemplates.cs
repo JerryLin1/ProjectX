3dd3c31c6f5ce68e34383768606f915178f631a5
p-han-tom <hantom0127@gmail.com>
2020-07-22 17:46:29 -0400

    Proc gen checkpoint

---
+++ Assets/RoomTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] downRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms; 
+
+}
---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+
+    public int openingDirection;
+    // 1 : needs top opening
+    // 2 : needs right opening
+    // 3 : down
+    // 4 : left
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        templates = GameObject.Find("RoomTemplates").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (!spawned) {
+            GameObject newRoom;
+            spawned = true;
+            if (openingDirection == 1) {
+                rand = Random.Range(0, templates.topRooms.Length);
+                newRoom = Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
+            } else if (openingDirection == 2) {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                newRoom = Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
+            } else if (openingDirection == 3) {
+                rand = Random.Range(0, templates.downRooms.Length);
+                newRoom = Instantiate(templates.downRooms[rand], transform.position, Quaternion.identity);
+            } else {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                newRoom = Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
+            }
+            newRoom.transform.SetParent(GameObject.Find("Grid").transform);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        
+    }
+
+}
